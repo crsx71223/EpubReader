e27d83985d2be2f66c98e08ebe4618418df892a2
Onur Keskin <keskion5415_aehit@students.vizja.pl>
2026-06-08 08:39:36 +0200

    feat: create zustand store for library management

---
+++ src/store/bookStore.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+import { getLibraryFiles } from "../utils/fileSystem";
+
+interface Book {
+  id: string;
+  title: string;
+  uri: string;
+}
+
+interface BookStore {
+  books: Book[];
+  isLoading: boolean;
+  loadBooks: () => Promise<void>;
+}
+
+export const useBookStore = create<BookStore>((set) => ({
+  books: [],
+  isLoading: false,
+
+  loadBooks: async () => {
+    set({ isLoading: true });
+    try {
+      const files = await getLibraryFiles();
+      set({ books: files, isLoading: false });
+    } catch (error) {
+      set({ books: [], isLoading: false });
+    }
+  },
+}));

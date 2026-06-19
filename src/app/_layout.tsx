94e56b779bfb56fec8deaa1ba776ebaf2c25f903
Onur Keskin <keskion5415_aehit@students.vizja.pl>
2026-06-19 07:50:42 +0200

    feat: wrap the navigation in gesture handler

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import ErrorBoundary from "../components/ErrorBoundary";
 import { Colors } from "../constants/theme";
 import { useSettingsStore } from "../store/settingsStore";
@@ -8,33 +9,35 @@ export default function RootLayout() {
   const theme = isDarkMode ? Colors.dark : Colors.light;
 
   return (
-    <ErrorBoundary>
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: theme.background },
-          headerTintColor: theme.text,
-          headerShadowVisible: false,
-        }}
-      >
-        <Stack.Screen
-          name="index"
-          options={{ headerShown: false, animation: "none" }}
-        />
-
-        <Stack.Screen
-          name="reader"
-          options={{ title: "Reader", animation: "none" }}
-        />
-
-        <Stack.Screen
-          name="settings"
-          options={{
-            presentation: "transparentModal",
-            animation: "fade",
-            headerShown: false,
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ErrorBoundary>
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: theme.background },
+            headerTintColor: theme.text,
+            headerShadowVisible: false,
           }}
-        />
-      </Stack>
-    </ErrorBoundary>
+        >
+          <Stack.Screen
+            name="index"
+            options={{ headerShown: false, animation: "none" }}
+          />
+
+          <Stack.Screen
+            name="reader"
+            options={{ title: "Reader", animation: "none" }}
+          />
+
+          <Stack.Screen
+            name="settings"
+            options={{
+              presentation: "transparentModal",
+              animation: "fade",
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </ErrorBoundary>
+    </GestureHandlerRootView>
   );
 }
